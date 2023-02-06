1af5f18af8f9acca78e0fc162d094d62e5f319f7
cormorecheleo <leocormo@gmail.com>
2023-02-06 13:19:33 +0100

    Initial Commit

---
+++ iCV/iCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCVApp.swift
+//  iCV
+//
+//  Created by Léo Cormoreche on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct iCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
